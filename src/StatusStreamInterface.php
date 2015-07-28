cbef6cc0ee01949bab92fbe1f8f53c852d0c1568
Charles Berube <charles.berube@cascadeenergy.com>
2015-07-28 09:18:23 -0700

    Doc comment update

--- src/StatusStreamInterface.php
+++ src/StatusStreamInterface.php
@@ -4,10 +4,39 @@ namespace Cascade\StatusStream;
 
 interface StatusStreamInterface
 {
-    public function update($state, $context = null);
-
+    /**
+     * Updates the current state to `active`, possibly with some context data
+     *
+     * @param mixed|null $context
+     */
     public function active($context = null);
+
+    /**
+     * Updates the current state to `degraded`, possibly with some context data
+     *
+     * @param mixed|null $context
+     */
     public function degraded($context = null);
+
+    /**
+     * Updates the current state to `failed`, possibly with some context data
+     *
+     * @param mixed|null $context
+     */
     public function failed($context = null);
+
+    /**
+     * Updates the current state to `idle`, possibly with some context data
+     *
+     * @param mixed|null $context
+     */
     public function idle($context = null);
+
+    /**
+     * Updates the current state to given state, possibly with some context data
+     *
+     * @param string $state
+     * @param mixed|null $context
+     */
+    public function update($state, $context = null);
 }
